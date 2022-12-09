6bd8418d684bc8e11db7d3dac42e0dec25c3bdf6
Krishnan Govindraj <krishnan@vaticle.com>
2022-12-09 13:26:32 +0000

    Remove comment

--- java/pattern/variable/UnboundVariable.java
+++ java/pattern/variable/UnboundVariable.java
@@ -31,7 +31,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class UnboundVariable extends Variable implements ConceptVariableBuilder,
-                                                         EvaluableVariableBuilder, // TODO: This isn't right, since it treats all syntactic variables as equal - Untrue, ConceptVariables and ValueVariables can't mix.
+                                                         EvaluableVariableBuilder,
                                                          TypeVariableBuilder,
                                                          ThingVariableBuilder.Common<ThingVariable.Thing>,
                                                          ThingVariableBuilder.Thing,
